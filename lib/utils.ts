13059039ebaa39c15237decbff694c7d7964aae1
Numulix <jbabic999@gmail.com>
2024-11-12 15:41:21 +0100

    feat(file-uploader): first progress on file uploader

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,72 @@ export function cn(...inputs: ClassValue[]) {
 export const parseStringify = (value: unknown) => {
   return JSON.parse(JSON.stringify(value));
 };
+
+export const getFileType = (fileName: string) => {
+  const extension = fileName.split(".").pop()?.toLowerCase();
+
+  if (!extension) return { type: "other", extension: "" };
+
+  const documentExtensions = [
+    "pdf",
+    "doc",
+    "docx",
+    "txt",
+    "xls",
+    "xlsx",
+    "csv",
+    "rtf",
+    "ods",
+    "ppt",
+    "odp",
+    "md",
+    "html",
+    "htm",
+    "epub",
+    "pages",
+    "fig",
+    "psd",
+    "ai",
+    "indd",
+    "xd",
+    "sketch",
+    "afdesign",
+    "afphoto",
+    "afphoto",
+  ];
+  const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "svg", "webp"];
+  const videoExtensions = ["mp4", "avi", "mov", "mkv", "webm"];
+  const audioExtensions = ["mp3", "wav", "ogg", "flac"];
+
+  if (documentExtensions.includes(extension))
+    return { type: "document", extension };
+  if (imageExtensions.includes(extension)) return { type: "image", extension };
+  if (videoExtensions.includes(extension)) return { type: "video", extension };
+  if (audioExtensions.includes(extension)) return { type: "audio", extension };
+
+  return { type: "other", extension };
+};
+
+export const convertFileToURL = (file: File) => URL.createObjectURL(file);
+
+export const getFileIcon = (extenstion: string, type: string) => {
+  const extensionIcons = [
+    "avi",
+    "csv",
+    "doc",
+    "mov",
+    "mp3",
+    "mp4",
+    "mpeg",
+    "pdf",
+    "svg",
+    "txt",
+    "wmv",
+    "xls",
+  ];
+
+  if (extensionIcons.includes(extenstion))
+    return `/assets/icons/extensions/${extenstion}.svg`;
+
+  return `/assets/icons/default/${type}.svg`;
+};
