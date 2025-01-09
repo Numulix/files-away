19b1cce35c4bcd071a149c088110e225e2421941
Numulix <jbabic999@gmail.com>
2025-01-09 10:15:33 +0100

    feat(file-actions): implement delete file

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -23,7 +23,11 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { renameFile, updatedFileUsers } from "@/lib/actions/file.actions";
+import {
+  deleteFile,
+  renameFile,
+  updatedFileUsers,
+} from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { FileDetails, ShareInput } from "./ActionsModalContent";
 
@@ -56,7 +60,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
       share: () => updatedFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log("delete"),
+      delete: () =>
+        deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path }),
     };
 
     success = await actions[action.value as keyof typeof actions]();
@@ -105,6 +110,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onInputChange={setEmails}
               onRemove={handleRemoveUser}
             />
+          )}
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete{` `}
+              <span className="delete-file-name">{file.name}?</span>
+            </p>
           )}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
--- types/index.d.ts
+++ types/index.d.ts
@@ -29,3 +29,9 @@ declare interface UpdateFileUsersProps {
   emails: string[];
   path: string;
 }
+
+declare interface DeleteFileProps {
+  fileId: string;
+  bucketFileId: string;
+  path: string;
+}
